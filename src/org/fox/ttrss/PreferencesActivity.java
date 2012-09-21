0570783c7631737fc901612eacb464eee681033e
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2012-09-21 12:13:42 +0400

    fix preferences not showing content

--- src/org/fox/ttrss/PreferencesActivity.java
+++ src/org/fox/ttrss/PreferencesActivity.java
@@ -1,7 +1,14 @@
 package org.fox.ttrss;
 
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class PreferencesActivity extends PreferenceActivity {
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        addPreferencesFromResource(R.xml.preferences);
+    }
 
 }
