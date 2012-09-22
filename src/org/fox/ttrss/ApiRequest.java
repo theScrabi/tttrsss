297259762228a2e7d7842d52074cf06c90cd7270
Andrew Dolgov <fox@fakecake.org>
2012-09-22 15:01:55 +0400

    fix SSL host verification not being disabled by "trust all hosts"

--- src/org/fox/ttrss/ApiRequest.java
+++ src/org/fox/ttrss/ApiRequest.java
@@ -12,8 +12,10 @@ import java.nio.CharBuffer;
 import java.security.cert.CertificateException;
 import java.util.HashMap;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
@@ -291,6 +293,12 @@ public class ApiRequest extends AsyncTask<HashMap<String,String>, Integer, JsonE
 	        sc.init(null, trustAllCerts, new java.security.SecureRandom());
 
 	        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+	        HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {				
+				@Override
+				public boolean verify(String hostname, SSLSession session) {
+					return true;
+				}
+			});
 
 	    } catch (Exception e) {
 	            e.printStackTrace();
--- src/org/fox/ttrss/FeedsFragment.java
+++ src/org/fox/ttrss/FeedsFragment.java
@@ -15,8 +15,10 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
@@ -606,6 +608,12 @@ public class FeedsFragment extends Fragment implements OnItemClickListener, OnSh
 		        sc.init(null, trustAllCerts, new java.security.SecureRandom());
 
 		        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		        HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {				
+					@Override
+					public boolean verify(String hostname, SSLSession session) {
+						return true;
+					}
+				});
 
 		    } catch (Exception e) {
 		            e.printStackTrace();
