f62363bc9600aade9154896c3249e80f155d0c20
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2014-10-30 15:15:47 +0400

    remove splitactionbar stuff from manifest

--- org.fox.ttrss/src/main/java/org/fox/ttrss/HeadlinesActivity.java
+++ org.fox.ttrss/src/main/java/org/fox/ttrss/HeadlinesActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
