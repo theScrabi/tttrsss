ff0d4c600518ca7859d9e0b5261f9a3450f7a154
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2015-02-09 17:03:35 +0300

    add version to preference xml

--- org.fox.ttrss/src/main/java/org/fox/ttrss/PreferencesActivity.java
+++ org.fox.ttrss/src/main/java/org/fox/ttrss/PreferencesActivity.java
@@ -1,6 +1,9 @@
 package org.fox.ttrss;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
 public class PreferencesActivity extends PreferenceActivity {
@@ -19,6 +22,20 @@ public class PreferencesActivity extends PreferenceActivity {
         if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.JELLY_BEAN) {
         	findPreference("enable_condensed_fonts").setEnabled(false);
         }
+
+        Preference versionPref = (Preference)findPreference("version");
+        String version = null;
+        int versionCode = -1;
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+
+            version = packageInfo.versionName;
+            versionCode = packageInfo.versionCode;
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        versionPref.setTitle(getString(R.string.version, version, versionCode));
     }
 
 }
