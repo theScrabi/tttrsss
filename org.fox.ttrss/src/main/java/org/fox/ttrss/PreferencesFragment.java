4582e6afefa4e2ebe9e036df3fd6e9b93cf8d3bc
Andrew Dolgov <noreply@madoka.volgo-balt.ru>
2017-06-01 12:19:13 +0300

    fix build timestamp

--- org.fox.ttrss/src/main/java/org/fox/ttrss/PreferencesFragment.java
+++ org.fox.ttrss/src/main/java/org/fox/ttrss/PreferencesFragment.java
@@ -9,6 +9,7 @@ import android.preference.PreferenceFragment;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -27,11 +28,11 @@ public class PreferencesFragment extends PreferenceFragment {
     public void onResume() {
         super.onResume();
 
-        String version = "?";
-        int versionCode = -1;
-        String buildTimestamp = "N/A";
-
         try {
+            String version;
+            int versionCode;
+            String buildTimestamp;
+
             Activity activity = getActivity();
 
             PackageInfo packageInfo = activity.getPackageManager().
@@ -40,23 +41,13 @@ public class PreferencesFragment extends PreferenceFragment {
             version = packageInfo.versionName;
             versionCode = packageInfo.versionCode;
 
-            ApplicationInfo appInfo = activity.getPackageManager().
-                    getApplicationInfo(activity.getPackageName(), 0);
-
-            ZipFile zf = new ZipFile(appInfo.sourceDir);
-            ZipEntry ze = zf.getEntry("classes.dex");
-            long time = ze.getTime();
-
-            buildTimestamp = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss",
-                    Locale.getDefault()).format(time);
-
-
             findPreference("version").setSummary(getString(R.string.prefs_version, version, versionCode));
+
+            buildTimestamp = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss").format(new Date(BuildConfig.TIMESTAMP));
+
             findPreference("build_timestamp").setSummary(getString(R.string.prefs_build_timestamp, buildTimestamp));
 
         } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
             e.printStackTrace();
         }
 
